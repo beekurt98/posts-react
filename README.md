e3e8a90b34dea2a03831bcd1a72eb65f25e85b38
Berna Kurt <beekurt98@gmail.com>
2025-01-16 19:09:49 +0300

    add css, fix selection, separate input component so the code is more dynamic

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# React + Vite
+# Comments and Replies with React + Vite
+
+Comments & replies are done using recursion.
+
+Todo:
+- Add open clicked photo as dialog
+
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,25 @@ import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import EmojiPicker from 'emoji-picker-react';
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+import { thumbsUp, thumbsDown, sortSvg, paperClip, imgSvg, emojiSvg, italicSvg, boldSvg, underlineSvg, replySvg } from './Svg';
 
-
-// todo: if liked, remove dislike & vice versa
+// todo:
 // open photo onclick as dialog
-// add bold etc to replies
 
 function App() {
   const [posts, setPosts] = useState([])
-  const [userName, setUserName] = useState("Bee")
+  const [userName, setUserName] = useState("Berna Kurt")
   // writing controls
-  const [emojiScreenOn, setEmojiScreen] = useState(false)
   const [input, setInput] = useState("")
   // img controls
   const [imgs, setImgs] = useState([])
   const [imgToShow, setImgToShow] = useState(null)
-  const [addImg, setAddImg] = useState(false)
   const textareaRef = useRef(null)
-  // link control
-  const [showLink, setShowLink] = useState(false)
 
   useEffect(() => {
     async function getData() {
       const data = await fetch('https://api.npoint.io/66575b019f7534b35ead').then(res => res.json())
+
       setPosts(data)
     }
 
@@ -55,103 +51,45 @@ function App() {
       return <Post setImgToShow={setImgToShow} imgs={imgs} userName={userName} setPosts={setPosts} posts={posts} key={crypto.randomUUID()} post={post} renderPosts={renderPosts} />
     })
   }
-
-  function handleEmoji(e) {
-    setInput(prevInput => prevInput + e.emoji)
+  function handleSelection() {
+    const start = textareaRef.current.selectionStart
+    const end = textareaRef.current.selectionEnd
+    const txt = textareaRef.current.value.substring(start, end)
+    return txt;
   }
 
-  function handleBold() {
-    let selectText = handleSelection()
-    setInput(input.replace(selectText, `**${selectText}**`))
-  }
-
-  function handleItalic() {
-    let selectText = handleSelection()
-    setInput(input.replace(selectText, `*${selectText}*`))
-  }
-
-  function handleUnderline() {
-    let selectText = handleSelection()
-    setInput(input.replace(selectText, `<u>${selectText}</u>`))
-  }
-
-  function handleLink(e) {
-    e.preventDefault();
-    const formData = new FormData(e.target)
-    const formObj = Object.fromEntries(formData)
-    console.log(formObj)
-    setInput(prevInput => prevInput + `[${formObj.linkTitle}](${formObj.linkUrl})`)
-
-    e.target.reset()
-  }
 
   function handleCommentInput(e) {
     setInput(e.target.value)
   }
 
-  function handleSelection() {
-    const start = textareaRef.current.selectionStart
-    const end = textareaRef.current.selectionEnd
-    const txt = input.slice(start, end)
-    // setSelectedText(txt)
-    return txt;
-  }
-
-  function handleAddImg(e) {
-    e.preventDefault();
-    const formData = new FormData(e.target)
-    const formObj = Object.fromEntries(formData)
-    console.log(formObj)
-    setInput(prevInput => prevInput + `![${formObj.imgAlt}](${formObj.imgUrl})`)
-
-    e.target.reset()
-  }
-
   return (
     <>
-      <h1>hey</h1>
-      <div className="write-comment">
-        <form onSubmit={handleCommentSubmit}>
-          <textarea ref={textareaRef} onSelect={handleSelection} onDoubleClick={handleSelection} autoComplete="off" className='comment-main-input' onChange={handleCommentInput} value={input} type="text" name="comment" id="" placeholder='Add comment...' ></textarea>
-          <input type="submit" value="Submit" />
-        </form>
-        <div className="control">
-          <button onClick={handleBold}>B</button>
-          <button onClick={handleItalic}><em>I</em></button>
-          <button onClick={handleUnderline}><u>U</u></button>
-          <span className="vertical-line"></span>
-          <button key={crypto.randomUUID()} onClick={() => {setShowLink(!showLink); setAddImg(false); setEmojiScreen(false)}}>🔗</button>
-          
-          <button key={crypto.randomUUID()} onClick={() => {setAddImg(!addImg); setShowLink(false); setEmojiScreen(false) }}>🖼️</button>
-          
-          <button onClick={(e) => { e.preventDefault(); setEmojiScreen(!emojiScreenOn); setShowLink(false); setAddImg(false) }}>☺️</button>
-          {
-            emojiScreenOn && <EmojiPicker onEmojiClick={handleEmoji} />
-          }
-          {
-            addImg && <div>
-            <form onSubmit={handleAddImg}>
-              <input type="text" placeholder='image alt' name='imgAlt' />
-              <input type="text" placeholder='image url' name="imgUrl" />
-              <input type="submit" name="" value="submit" />
-            </form>
-          </div>
-          }
-          {
-            showLink && <div>
-              <form onSubmit={handleLink}>
-                <input type="text" placeholder='link title' name='linkTitle' />
-                <input type="text" placeholder='link url' name="linkUrl" />
-                <input type="submit" name="" value="submit" />
-              </form>
+      <div className="main-container">
+        <div className="write-comment">
+          <form onSubmit={handleCommentSubmit}>
+            <textarea ref={textareaRef} onSelect={handleSelection} onDoubleClick={handleSelection} autoComplete="off" className='comment-main-input' onChange={handleCommentInput} value={input} type="text" name="comment" id="" placeholder='Add comment...' ></textarea>
+            <input type="submit" value="Submit" className='comment-submit-btn' />
+          </form>
+          <InputControlField handleSelection={handleSelection} textareaRef={textareaRef} input={input} setInput={setInput} />
+        </div>
+        <div className="posts-div">
+          <div className="header">
+            <h2>Comments <span className='comment-count'>{posts.length}</span></h2>
+            <div className="header-btns">
+              <button className="sort-btn">{sortSvg}</button>
+              <select name="order" id="">
+                <option value="MostRecent">Most Recent</option>
+              </select>
             </div>
-          }
+          </div>
+          <div className="comments-container">
+            {renderPosts(posts)}
+          </div>
         </div>
       </div>
-      <div className="posts-div">
-        {renderPosts(posts)}
-      </div>
-      <dialog > 
+
+      <dialog >
         {imgToShow}
       </dialog>
     </>
@@ -164,10 +102,11 @@ function Post({ setImgToShow, userName, imgs, post, renderPosts, posts, setPosts
   const [dislikeCount, setDislikeCount] = useState(post.dislikes)
   const [commentLiked, setCommentLiked] = useState(false)
   const [commentDisliked, setCommentDisliked] = useState(false)
-
+  const textareaReplyRef = useRef(null)
   // reply controls
   const [replying, setReplying] = useState(false)
   const [replies, setReplies] = useState([post.replies][0])
+  const [replyInput, setReplyInput] = useState("")
 
 
   function handleLikes() {
@@ -197,34 +136,145 @@ function Post({ setImgToShow, userName, imgs, post, renderPosts, posts, setPosts
       replies: [],
     }
     setReplies([...replies, newReply])
-    // console.log(replies)
     e.target.reset();
     const thisPost = posts.find(x => x.comment == post.comment)
-    console.log(thisPost.replies)
     thisPost.replies.push(newReply)
+    setReplyInput("")
+    setReplying(false)
   }
 
-
+  function handleCommentInput(e) {
+    setReplyInput(e.target.value)
+  }
+  console.log("post", post)
   return (
     <>
       <div className='comment'>
-        <img width={50} src="https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg" />
-        <h3>{post.name}</h3> <span>{post.time}</span>
-        <div className='comment-content' dangerouslySetInnerHTML={{ __html: marked.parse(post.comment) }} />
-        
-        <button className={commentLiked ? "liked" : ""} onClick={handleLikes} >👍 {likeCount}</button>
-        <button className={commentDisliked ? "disliked" : ""} onClick={handleDislikes}>👎 {dislikeCount}</button>
-        <button className="reply-btn" onClick={() => setReplying(!replying)}>🗩 Reply</button>
-        {
-          replying && <div className="reply-div">
-            <form onSubmit={handleReply}>
-              <input type="text" name='comment' placeholder='Add reply...' />
-              <input type="submit" value="Submit" />
-            </form>
+        <div className="comment-left">
+          <img width={50} src={`https://ui-avatars.com/api/?name=${post.name.split(" ")[0]}+${post.name.split(" ")[1]}&background=random&bold=true&color=fff`} />
+        </div>
+        <div className="comment-right">
+
+          <h3>{post.name}<span>{post.time}</span></h3>
+          <div className='comment-content' dangerouslySetInnerHTML={{ __html: marked.parse(post.comment) }} />
+          <div className="feedback-cont">
+            <div className={commentLiked ? "liked feedback-div" : "feedback-div"}>
+              <button className="feedback-btn" onClick={handleLikes} >{thumbsUp}</button> <p className="feedback-count">{likeCount}</p>
+            </div>
+            <div className={commentDisliked ? "disliked feedback-div" : "feedback-div"}>
+              <button className="feedback-btn" onClick={handleDislikes}>{thumbsDown} </button><p className="feedback-count">{dislikeCount}</p>
+            </div>
+            <div className="feedback-div reply-btn" onClick={() => setReplying(!replying)}>
+              <div className="" >{replySvg}</div><p>Reply</p>
+            </div>
           </div>
-        }
-        {post.replies && renderPosts(post.replies)}
+          {
+            replying && <><div className="reply-div">
+              <form onSubmit={handleReply}>
+                <textarea ref={textareaReplyRef} onChange={handleCommentInput} value={replyInput} type="text" name='comment' placeholder='Add reply...'> </textarea>
+                <input type="submit" value="Submit" className='comment-submit-btn' />
+              </form>
+            
+              <InputControlField textareaRef={textareaReplyRef} input={replyInput} setInput={setReplyInput} />
+              </div>
+            </>
+
+          }
+          <div className='replies-container'>
+            {post.replies && renderPosts(post.replies)}
+          </div>
+        </div>
       </div>
+
+    </>
+  )
+}
+
+function InputControlField({ textareaRef, input, setInput }) {
+  const [addImg, setAddImg] = useState(false)
+  const [showLink, setShowLink] = useState(false)
+  const [emojiScreenOn, setEmojiScreen] = useState(false)
+
+  function handleEmoji(e) {
+    setInput(prevInput => prevInput + e.emoji)
+  }
+
+  function handleSelection(styling) {
+    const textarea = textareaRef.current
+    const start = textarea.selectionStart
+    const end = textarea.selectionEnd
+
+    if (start === end) return;
+
+
+    const beforeSelection = textarea.value.substring(0, start);
+    const selectedText = textarea.value.substring(start, end)
+    const afterSelection = textarea.value.substring(end);
+
+    const textSyling = {
+      "bold": `**${selectedText}**`,
+      "italic": `*${selectedText}*`,
+      "underline": `<u>${selectedText}</u>`
+    }
+    // const boldedText = `**${selectedText}**`
+
+    setInput(beforeSelection + textSyling[styling] + afterSelection)
+  }
+
+  function handleLink(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target)
+    const formObj = Object.fromEntries(formData)
+    setInput(prevInput => prevInput + `[${formObj.linkTitle}](${formObj.linkUrl})`)
+
+    e.target.reset()
+  }
+
+  function handleAddImg(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target)
+    const formObj = Object.fromEntries(formData)
+    setInput(prevInput => prevInput + `![${formObj.imgAlt}](${formObj.imgUrl})`)
+
+    e.target.reset()
+  }
+  return (
+    <>
+      <div className="control">
+        <button className="icon-button" type='button' onClick={() => handleSelection("bold")}>{boldSvg}</button>
+        <button className="icon-button" type='button' onClick={() => handleSelection("italic")}>{italicSvg}</button>
+        <button className="icon-button" type='button' onClick={() => handleSelection("underline")}>{underlineSvg}</button>
+        <span className="vertical-line"></span>
+        <button className="icon-button" type='button' key={crypto.randomUUID()} onClick={() => { setShowLink(!showLink); setAddImg(false); setEmojiScreen(false) }}>{paperClip}</button>
+
+        <button className="icon-button" type='button' key={crypto.randomUUID()} onClick={() => { setAddImg(!addImg); setShowLink(false); setEmojiScreen(false) }}>{imgSvg}</button>
+
+        <button className="icon-button" type='button' onClick={(e) => { e.preventDefault(); setEmojiScreen(!emojiScreenOn); setShowLink(false); setAddImg(false) }}>{emojiSvg}</button>
+
+      </div>
+      {
+        emojiScreenOn && <EmojiPicker onEmojiClick={handleEmoji} />
+      }
+      {
+        addImg && <div className='addMedia'>
+          <div className="icon-button" >{imgSvg}</div>
+          <form onSubmit={handleAddImg}>
+            <input type="text" placeholder='Image alt' name='imgAlt' />
+            <input type="url" placeholder='Image url' name="imgUrl" />
+            <input type="submit" name="" value="Add" />
+          </form>
+        </div>
+      }
+      {
+        showLink && <div className='addMedia'>
+          <div className="icon-button" >{paperClip}</div>
+          <form onSubmit={handleLink}>
+            <input type="text" placeholder='Link title' name='linkTitle' />
+            <input type="url" placeholder='Link url' name="linkUrl" />
+            <input type="submit" name="" value="Add" />
+          </form>
+        </div>
+      }
 
     </>
   )
